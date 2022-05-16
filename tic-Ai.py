665e8a5d1b8fa7ca87eb9d271d1c5cee922d89ed
capstone-project404 <102313165+capstone-project404@users.noreply.github.com>
2022-05-16 07:45:43 -0700

    Update tic-Ai.py

--- tic-Ai.py
+++ tic-Ai.py
@@ -7,11 +7,11 @@ board = [' ' for i in range(10)]
 
 def print_board():
   print('#########################')
-  print(board[1] + '|' + board[2] + '|' + board[3])
+  print(board[7] + '|' + board[8] + '|' + board[9])
   print('------')
   print(board[4] + '|' + board[5] + '|' + board[6])
   print('------')
-  print(board[7] + '|' + board[8] + '|' + board[9])
+  print(board[1] + '|' + board[2] + '|' + board[3])
 
 
 def is_full():
